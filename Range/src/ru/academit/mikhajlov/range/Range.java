bd94965f3c15220c4259f8b2f49daf0c2ced2506
Pavel <pascha.mixajloff2016@yandex.ru>
2019-01-19 14:26:28 +0700

    Задача Range*

--- Range/src/ru/academit/mikhajlov/range/Range.java
+++ Range/src/ru/academit/mikhajlov/range/Range.java
@@ -29,51 +29,36 @@ public class Range {
         return to - from;
     }
 
-    private boolean isInside(double number) {
+    public boolean isInside(double number) {
         return (number >= from) && (number <= to);
     }
 
     public Range getIntersection(Range range) {
-        if (isInside(range.to) && (from <= range.from)) {
-            return new Range(range.from, range.to);
-        } else if ((range.isInside(to) && (from >= range.from))) {
-            return new Range(from, to);
-        } else {
+        if (Math.max(from, range.from) >= Math.min(to, range.to)) {
             return null;
+        } else {
+            return new Range(Math.max(from, range.from), Math.min(to, range.to));
         }
     }
 
     public Range[] getUnion(Range range) {
-        if (Math.max(from, range.from) < Math.min(to, range.to)) {
-            // if (isInside(range.from) && isInside(range.to))
-            if (to > range.to && range.from > from) {
-                return new Range[]{new Range(from, to)};
-            } else if (to < range.to && range.from < from) {
-                return new Range[]{new Range(range.from, range.to)};
-            }
-            return new Range[]{new Range(Math.max(from, range.from), Math.min(to, range.to))};
+        if (Math.max(from, range.from) > Math.min(to, range.to)) {
+            return new Range[]{new Range(Math.min(from, range.from), Math.min(to, range.to)), new Range(Math.max(from, range.from), Math.max(to, range.to))};
         } else {
-            return new Range[]{new Range(from, to), new Range(range.from, range.to)};
+            return new Range[]{new Range(Math.min(from, range.from), Math.max(to, range.to))};
         }
     }
 
     public Range[] getDifference(Range range) {
-        double from = this.from;
-        double to = this.to;
-        double from1 = range.from;
-        double to1 = range.to;
-
-        if (from1 > to || from > to1) { // Пересечения нет
+        if (range.from > to || from > range.to) {
             return new Range[]{new Range(from, to)};
-        } else if (from >= from1 && to >= to1 /*&& from <= to1*/) { // пересечение по одному концу
-            return new Range[]{new Range(from, to)};
-        }
-        if (from1 >= from && to1 >= to) { // Пересечение по одному концу
-            return new Range[]{new Range(from, to)};
-        }
-        if (from1 > from && to > to1) {
-            return new Range[]{new Range(from, from1), new Range(to1, to)};
-        } else { // Пустое множество
+        } else if (from >= range.from && to >= range.to) {
+            return new Range[]{new Range(range.to, to)};
+        } else if (range.from >= from && range.to >= to) {
+            return new Range[]{new Range(from, range.from)};
+        } else if (range.from > from && to > range.to) {
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+        } else {
             return new Range[]{};
         }
     }
