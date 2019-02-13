dcf1af17d438aed5cc90b52f0f631dc59fd7888c
Pavel <pascha.mixajloff2016@yandex.ru>
2019-02-14 03:15:17 +0700

    Задача Matrix сохранение от 14.02.19

--- Matrix/src/ru/academit/mikhajlov/main/Main.java
+++ Matrix/src/ru/academit/mikhajlov/main/Main.java
@@ -8,25 +8,36 @@ public class Main {
         Matrix matrix1 = new Matrix(3, 3);
 
 
-        double[][] arrays = new double[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] arrays = new double[][]{{5, 8, -4}, {6, 9, -5}, {4, 7, -3}};
+        double[][] arrays1 = new double[][]{{3, 2, 5}, {4, -1, 3}, {9, 6, 5}};
+
 
         Vector[] lines = new Vector[]{new Vector(new double[]{3, -3, 5, 5}), new Vector(new double[]{2, -3, 2, 3}),
                 new Vector(new double[]{4, -7, 3, 4}), new Vector(new double[]{5, -5, 3, 8})};
         Vector[] lines1 = new Vector[]{new Vector(new double[]{1, 2, 3}), new Vector(new double[]{4, 5, 6})};
 
-        double[] numbers = new double[]{2, 2, 2};
+        double[] numbers = new double[]{2, -3, 1};
         Vector vector = new Vector(numbers);
 
         Matrix matrix2 = new Matrix(arrays);
-        matrix2.multiplicationOnScalar(vector);
+        matrix2.multiplicationOnVector(vector);
 
         Matrix matrix3 = new Matrix(lines);
+        matrix3.transposition();
 
+        Matrix matrix4 = new Matrix(arrays);
+        Matrix matrix5 = new Matrix(arrays);
+        matrix4.add(matrix5);
+
+        Matrix matrix6 = new Matrix(arrays);
+        Matrix matrix7 = new Matrix(arrays1);
+        Matrix matrix8 = Matrix.multiplication(matrix6, matrix7);
 
         System.out.println(matrix3.getLine(1).toString());
         System.out.println(matrix3.getColumn(1).toString());
         System.out.println(matrix3.getDeterminant());
-//        System.out.println(matrix2.toString());
-//        System.out.println(matrix3.toString());
+        System.out.println(matrix2.toString());
+        System.out.println(matrix4.toString());
+        System.out.println(matrix8.toString());
     }
 }
--- Matrix/src/ru/academit/mikhajlov/matrix/Matrix.java
+++ Matrix/src/ru/academit/mikhajlov/matrix/Matrix.java
@@ -82,21 +82,23 @@ public class Matrix {
     }
 
     public double getDeterminant() {
-        if (this.getHeight() != this.getWidth()) {
+        int width = getWidth();
+        int height = getHeight();
+
+        if (height != width) {
             throw new IllegalArgumentException("Высота и ширина матрицы должны быть одинаковые.");
         }
-        if (this.getHeight() == 2 && this.getWidth() == 2) {
+        if (height == 2) {
             return (this.lines[0].getComponent(0) * this.lines[1].getComponent(1)) -
                     (this.lines[0].getComponent(1) * this.lines[1].getComponent(0));
         }
-
         Matrix copy = new Matrix(this);
         double determinant = 0;
-        for (int i = 0; i < this.getHeight(); i++) {
-            double[][] tmp = new double[this.getHeight() - 1][this.getWidth() - 1];
-            for (int j = 1; j < this.getWidth(); j++) {
+        for (int i = 0; i < height; i++) {
+            double[][] tmp = new double[height - 1][width - 1];
+            for (int j = 1; j < width; j++) {
                 int columnIndex = 0;
-                for (int m = 0; m < this.getWidth(); m++) {
+                for (int m = 0; m < width; m++) {
                     if (m == i) {
                         continue;
                     }
@@ -109,23 +111,65 @@ public class Matrix {
         return determinant;
     }
 
-    public void multiplicationOnVector (Vector vector) {
-        if (this.getWidth() != vector.getSize()) {
+    public void multiplicationOnVector(Vector vector) {
+        int length = vector.getSize();
+        if (getWidth() != length) {
             throw new IllegalArgumentException("Ширина матрицы и размерность вектора должны быть равны.");
         }
-        Vector[] array = new Vector[] {vector};
-        Matrix matrix = new Matrix(array);
-        matrix.transposition();
-        Matrix matrixT = new Matrix(matrix);
-        Matrix productResult = new Matrix(vector.getSize(), 1);
-        for (int i = 0; i < getHeight(); i++) {
-            double componentsSum = 0;
-            for (int j = 0; j < getWidth(); j++) {
-                componentsSum += this.lines[i].getComponent(j) * matrixT.lines[j].getComponent(0);
-            }
-            productResult.lines[i].setComponent(componentsSum, 0);
+        for (int i = 0; i < length; i++) {
+            double[] vectorComponent = new double[] {Vector.getScalarProduct(getLine(i), vector)};
+            Vector tmp = new Vector(vectorComponent);
+            lines[i] = tmp;
         }
-        this.lines = productResult.lines;
+    }
+
+    public void add(Matrix matrix) {
+        if ((getHeight() != matrix.getHeight()) || (getWidth() != matrix.getWidth())) {
+            throw new IllegalArgumentException("Размер матрмц должен быть одинаковым.");
+        }
+
+        for (int i = 0; i < getHeight(); i++) {
+            for (int j = 0; j < getWidth(); j++) {
+                lines[i].setComponent(lines[i].getComponent(j) + matrix.lines[i].getComponent(j), j);
+            }
+        }
+    }
+
+    public void deduction(Matrix matrix) {
+        if ((getHeight() != matrix.getHeight()) || (getWidth() != matrix.getWidth())) {
+            throw new IllegalArgumentException("Размер матрмц должен быть одинаковым.");
+        }
+
+        for (int i = 0; i < getHeight(); i++) {
+            for (int j = 0; j < getWidth(); j++) {
+                lines[i].setComponent(lines[i].getComponent(j) - matrix.lines[i].getComponent(j), j);
+            }
+        }
+    }
+
+    public static Matrix sum(Matrix matrix1, Matrix matrix2) {
+        Matrix matrix1Copy = new Matrix(matrix1);
+        matrix1Copy.add(matrix2);
+        return matrix1Copy;
+    }
+
+    public static Matrix difference(Matrix matrix1, Matrix matrix2) {
+        Matrix matrix1Copy = new Matrix(matrix1);
+        matrix1Copy.deduction(matrix2);
+        return matrix1Copy;
+    }
+
+    public static Matrix multiplication(Matrix matrix1, Matrix matrix2) {
+        Matrix result = new Matrix(matrix2.getHeight(), matrix2.getWidth());
+        for (int i = 0; i < matrix2.getHeight(); i++) {
+            Vector vector = matrix2.getColumn(i);
+            Matrix tmp = new Matrix(matrix1);
+            tmp.multiplicationOnVector(vector);
+            Vector tmpVector = new Vector(tmp.getColumn(0));
+            result.lines[i] = tmpVector;
+        }
+        result.transposition();
+        return new Matrix(result);
     }
 }
 
