998b6d4d0cfcbc76f401109aa8512e65a2f6c9c0
Pavel <pascha.mixajloff2016@yandex.ru>
2019-03-04 21:15:03 +0700

    Задача CSV сохранение частичного исправления замечаний 04.03.2019

--- CSV/src/ru/academit/mikhajlov/main/CsvConverter.java
+++ CSV/src/ru/academit/mikhajlov/main/CsvConverter.java
@@ -10,36 +10,41 @@ public class CsvConverter {
         try (PrintWriter writer = new PrintWriter("table.html");
              Scanner scanner = new Scanner(new FileInputStream("РазборCSV.csv"), "windows-1251")) {
             writer.println("<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>HTML table</title></head><body><table border=\"1\">");
+
             if (scanner.hasNextLine()) {
                 writer.print("<tr><td>");
             }
             int quotesCount = 0;
             boolean isInQuotes;
+
             while (scanner.hasNextLine()) {
                 String row = scanner.nextLine();
-                for (int i = 0; i <= row.length() - 1; i++) {
-                    if (row.charAt(i) == '>') {
-                        writer.print("&gt");
+                for (int i = 0; i < row.length(); i++) {
+                    char currentChar = row.charAt(i);
+
+                    if (currentChar == '>') {
+                        writer.print("&gt;");
                         continue;
-                    } else if (row.charAt(i) == '<') {
-                        writer.print("&lt");
+                    } else if (currentChar == '<') {
+                        writer.print("&lt;");
                         continue;
-                    } else if (row.charAt(i) == '&') {
-                        writer.print("&amp");
+                    } else if (currentChar == '&') {
+                        writer.print("&amp;");
                         continue;
                     }
-                    if (row.charAt(i) == '"') {
+                    if (currentChar == '"') {
                         ++quotesCount;
                     }
+
                     isInQuotes = quotesCount % 2 != 0;
                     if (!isInQuotes) {
-                        if (row.charAt(i) == '"') {
+                        if (currentChar == '"') {
                             continue;
                         }
-                        if (row.charAt(i) == ',') {
+                        if (currentChar == ',') {
                             writer.println("</td><td>");
                         } else {
-                            writer.print(row.charAt(i));
+                            writer.print(currentChar);
                         }
                         if (i == row.length() - 1) {
                             writer.println("</td></tr><tr><td>");
@@ -48,21 +53,22 @@ public class CsvConverter {
                         if (i == 0) {
                             writer.print("<br/>");
                         }
-                        if (row.charAt(i) == '"') {
+                        if (currentChar == '"') {
                             if (i != 0 && row.charAt(i - 1) != ',') {
                                 writer.print("\"");
                             }
-                        } else if (row.charAt(i) == ',') {
+                        } else if (currentChar == ',') {
                             writer.print(",");
                         } else {
-                            writer.print(row.charAt(i));
+                            writer.print(currentChar);
                         }
                     }
                 }
             }
-            if (!scanner.hasNextLine()) {
-                writer.println("</tr></table></body></html>");
-            }
+            writer.println("</td></tr></table></body></html>");
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
+            throw e;
         }
     }
 }
