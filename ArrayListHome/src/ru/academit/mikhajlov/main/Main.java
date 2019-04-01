57f75a138c9e34d6ec8f3278fb7af9bbb8c39bf8
Pavel <pascha.mixajloff2016@yandex.ru>
2019-04-01 16:52:20 +0700

    Задача ArrayListHome.

--- ArrayListHome/src/ru/academit/mikhajlov/main/Main.java
+++ ArrayListHome/src/ru/academit/mikhajlov/main/Main.java
@@ -3,6 +3,7 @@ package ru.academit.mikhajlov.main;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -12,44 +13,39 @@ public class Main {
             return;
         }
 
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
         try (Scanner scanner = new Scanner(new FileInputStream(args[0]), "windows-1251")) {
-            while (scanner.hasNextInt()) {
-                list.add(scanner.nextInt());
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
             }
         }
         System.out.println(list);
 
         // Первый вариант.
-        if (!list.isEmpty()) {
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i) % 2 == 0) {
-                    list.remove(i);
-                    i--;
-                }
+        ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(5, 2, 8, 5, 9, 14, 4, 2, 15, 9, 22, 10));
+        for (int i = 0; i < numbersList.size(); i++) {
+            if (numbersList.get(i) % 2 == 0) {
+                numbersList.remove(i);
+                i--;
             }
-            System.out.println(list);
         }
+        System.out.println(numbersList);
 
         //Второй вариант.
-        if (!list.isEmpty()) {
-            for (int i = list.size() - 1; i >= 0; i--) {
-                if (list.get(i) % 2 == 0) {
-                    list.remove(i);
-                }
+        for (int i = numbersList.size() - 1; i >= 0; i--) {
+            if (numbersList.get(i) % 2 == 0) {
+                numbersList.remove(i);
             }
-            System.out.println(list);
         }
+        System.out.println(numbersList);
 
         ArrayList<Integer> list2 = new ArrayList<>();
-        if (!list.isEmpty()) {
-            for (int i = 0; i < list.size(); i++) {
-                if (list.indexOf(list.get(i)) == list.lastIndexOf(list.get(i))) {
-                    list2.add(list.get(i));
-                }
+        for (Integer integer : numbersList) {
+            if (!list2.contains(integer)) {
+                list2.add(integer);
             }
-            System.out.println(list2);
         }
+        System.out.println(list2);
     }
 }
 
