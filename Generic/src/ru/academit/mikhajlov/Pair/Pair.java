c542d3955e7761b61b822d59b07c835e67b3f1fa
Pavel <pascha.mixajloff2016@yandex.ru>
2019-04-06 20:54:22 +0700

    Задача Generic(Class work).

---
+++ Generic/src/ru/academit/mikhajlov/Pair/Pair.java
@@ -0,0 +1,33 @@
+package ru.academit.mikhajlov.Pair;
+
+public class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public class Util {
+        public <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
+            return p1.getKey().equals(p2.getKey()) && p1.getValue().equals(p2.getValue());
+        }
+    }
+}
