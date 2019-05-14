ab75248fe5bf8fe35d7b1573ebe06ca00d41e7d6
Pavel <pascha.mixajloff2016@yandex.ru>
2019-05-14 10:53:20 +0700

    Задача Lyambda исправление замечания.

--- Lyambda/src/ru/academit/mikhajlov/Person.java
+++ Lyambda/src/ru/academit/mikhajlov/Person.java
@@ -20,10 +20,14 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
-
+//
     public void setAge(Integer age) {
         this.age = age;
     }
 
-
+    @Override
+    public String toString() {
+        return "Name: " +  name + "; " + "Age: " + age + "; ";
+    }
 }
+
--- Lyambda/src/ru/academit/mikhajlov/main/Main.java
+++ Lyambda/src/ru/academit/mikhajlov/main/Main.java
@@ -3,11 +3,23 @@ package ru.academit.mikhajlov.main;
 import ru.academit.mikhajlov.Person;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List list = new ArrayList<Person>();
-        list.add("");
+        Person person1 = new Person("Pavel", 33);
+        Person person2 = new Person("Aleksey", 26);
+        Person person3 = new Person("Tatyana", 29);
+        Person person4 = new Person("Aleksandr", 43);
+        Person person5 = new Person("Elena", 33);
+
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(person1);
+        persons.add(person2);
+        persons.add(person3);
+        persons.add(person4);
+        persons.add(person5);
+        Stream<Person> stream = persons.stream().filter(person -> person.getAge() > 30);
+        stream.forEach(System.out::println);
     }
 }
