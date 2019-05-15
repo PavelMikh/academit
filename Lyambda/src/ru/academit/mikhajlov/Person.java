214d17e5a222c69283b5e4a88001c3f709fcde07
Pavel <pascha.mixajloff2016@yandex.ru>
2019-05-15 18:19:49 +0700

    Задача Lyambda.

--- Lyambda/src/ru/academit/mikhajlov/Person.java
+++ Lyambda/src/ru/academit/mikhajlov/Person.java
@@ -17,14 +17,6 @@ public class Person {
         return age;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-//
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
     @Override
     public String toString() {
         return "Name: " +  name + "; " + "Age: " + age + "; ";
--- Lyambda/src/ru/academit/mikhajlov/main/Main.java
+++ Lyambda/src/ru/academit/mikhajlov/main/Main.java
@@ -3,6 +3,12 @@ package ru.academit.mikhajlov.main;
 import ru.academit.mikhajlov.Person;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Main {
@@ -11,7 +17,11 @@ public class Main {
         Person person2 = new Person("Aleksey", 26);
         Person person3 = new Person("Tatyana", 29);
         Person person4 = new Person("Aleksandr", 43);
-        Person person5 = new Person("Elena", 33);
+        Person person5 = new Person("Aleksandr", 31);
+        Person person6 = new Person("Elena", 33);
+        Person person7 = new Person("Kirill", 3);
+        Person person8 = new Person("Leyla", 16);
+        Person person9 = new Person("Grigoriy", 6);
 
         ArrayList<Person> persons = new ArrayList<>();
         persons.add(person1);
@@ -19,7 +29,38 @@ public class Main {
         persons.add(person3);
         persons.add(person4);
         persons.add(person5);
-        Stream<Person> stream = persons.stream().filter(person -> person.getAge() >= 29);
-        stream.forEach(System.out::println);
+        persons.add(person6);
+        persons.add(person7);
+        persons.add(person8);
+        persons.add(person9);
+
+        Stream<String> stream = persons.stream().map(Person::getName).distinct();
+        System.out.printf("Names: %s. ", stream.collect(Collectors.joining(", ")));
+        System.out.println();
+
+        System.out.printf("Average age of persons under 18 = %s.%n", persons.stream().filter(person -> person.getAge() < 18).mapToDouble(Person::getAge).average());
+
+        //Map<K, V> K - names, V - averageAge
+        Map<String, Double> map = persons.stream().collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
+        map.forEach((name, age) -> System.out.printf("name %11s: average age = %s%n", name, age));
+
+        System.out.println("List of people from 20 to 45 years sorted descending: " + persons.stream().filter(person -> person.getAge() >= 20 && person.getAge() <= 45)
+                .sorted(Comparator.comparingInt(Person::getAge).reversed())
+                .map(Person::getName)
+                .collect(Collectors.joining(", ")));
+
+        //Creating an infinite stream of square number roots
+        System.out.println("Enter the count of numbers you want to calculate: ");
+        DoubleStream sqrt = DoubleStream.iterate(0, number -> number + 1)
+                .map(Math::sqrt)
+                .limit(new Scanner(System.in).nextInt());
+        sqrt.forEach(System.out::println);
+
+        //Creating an infinite stream of Fibonacci numbers
+        System.out.println("Enter the count of Fibonacci numbers you want calculate: ");
+        LongStream fibonacci = LongStream.iterate(0, number -> number + 1)
+                .map(number -> (long) (Math.pow(((Math.sqrt(5) + 1) / 2), number) / Math.sqrt(5) + 0.5))
+                .limit(new Scanner(System.in).nextInt());
+        fibonacci.forEach(System.out::println);
     }
 }
