ea41f05cd3b01183149fdfea07c7d75d421f6ab3
Pavel <pascha.mixajloff2016@yandex.ru>
2019-05-21 01:21:27 +0700

    Задача Перевод температур сохранение.

--- TemperatureConverter/src/ru/academit/mikhajlov/Converter/Temperatures.java
+++ TemperatureConverter/src/ru/academit/mikhajlov/Converter/Temperatures.java
@@ -2,7 +2,10 @@ package ru.academit.mikhajlov.Converter;
 
 interface Temperatures {
     double getValue();
+
     void setCelsius(double value);
+
     void setFahrenheit(double value);
+
     void setKelvin(double value);
 }
