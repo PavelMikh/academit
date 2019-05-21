5ed1a48024c5c689a761a49a5f1b0b155de11bbf
Pavel <pascha.mixajloff2016@yandex.ru>
2019-05-22 01:45:57 +0700

    Задача Перевод температур сохранение.

--- TemperatureConverter/src/ru/academit/mikhajlov/Converter/Converter.java
+++ TemperatureConverter/src/ru/academit/mikhajlov/Converter/Converter.java
@@ -1,8 +1,29 @@
 package ru.academit.mikhajlov.Converter;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Converter extends JFrame implements Temperatures {
+    private JFrame frame;
+    JPanel textPanel;
+    JPanel functionalPanel;
+    JComboBox<String> fromType;
+    JComboBox<String> toType;
+    JTextField inputField;
+    JTextField outputField;
+    JButton calculationButton;
+
+    public Converter() {
+        //Создаётся окно определённого размера с названием и иконкой.
+        frame = new JFrame("Конвертер температур");
+        frame.setLocationRelativeTo(null);
+        Image image = Toolkit.getDefaultToolkit().getImage("градусник.img");
+        frame.setIconImage(image);
+        frame.setSize(300, 90);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
     @Override
     public double getValue() {
         return 0;
