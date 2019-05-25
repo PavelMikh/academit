a00ae9d5de4139e59b57ebf3f87bfabc06c1da02
Pavel <pascha.mixajloff2016@yandex.ru>
2019-05-25 20:12:01 +0700

    Задача Перевод температур сохранение.

--- TemperatureConverter/src/ru/academit/mikhajlov/Converter/CelsiusListener.java
+++ TemperatureConverter/src/ru/academit/mikhajlov/Converter/CelsiusListener.java
@@ -3,16 +3,19 @@ package ru.academit.mikhajlov.Converter;
 import ru.academit.mikhajlov.Annotation.Annotation;
 
 public class CelsiusListener implements Listener {
+    @Override
     @Annotation.Init(flag = "Цельсий")
     public double toCelsius(double value) {
         return value;
     }
 
+    @Override
     @Annotation.Init(flag = "Фаренгейт")
     public double toFahrenheit(double value) {
         return value * 1.8 + 32;
     }
 
+    @Override
     @Annotation.Init(flag = "Кельвин")
     public double toKelvin(double value) {
         return value + 273.15;
--- TemperatureConverter/src/ru/academit/mikhajlov/Converter/KelvinListener.java
+++ TemperatureConverter/src/ru/academit/mikhajlov/Converter/KelvinListener.java
@@ -3,16 +3,19 @@ package ru.academit.mikhajlov.Converter;
 import ru.academit.mikhajlov.Annotation.Annotation;
 
 public class KelvinListener implements Listener {
+    @Override
     @Annotation.Init(flag = "Цельсий")
     public double toCelsius(double value) {
         return value - 273.15;
     }
 
+    @Override
     @Annotation.Init(flag = "Фаренгейт")
     public double toFahrenheit(double value) {
         return (value - 273.15) * 1.8 + 32;
     }
 
+    @Override
     @Annotation.Init(flag = "Кельвин")
     public double toKelvin(double value) {
         return value;
