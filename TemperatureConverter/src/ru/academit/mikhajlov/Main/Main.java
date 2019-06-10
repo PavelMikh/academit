45f0b2d0cecd0f2abf706a0d1f926df860119ac9
pavel <pamix1985@gmail.com>
2019-06-10 11:06:40 +0700

    Задача TemperatureConverter исправление замечаний.

--- TemperatureConverter/src/ru/academit/mikhajlov/Main/Main.java
+++ TemperatureConverter/src/ru/academit/mikhajlov/Main/Main.java
@@ -2,8 +2,6 @@ package ru.academit.mikhajlov.Main;
 
 import ru.academit.mikhajlov.View.TemperatureConverter;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
         //noinspection unused
